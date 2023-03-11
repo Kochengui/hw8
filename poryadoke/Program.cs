cce98636045c9cdc3e1eff0bb158772e7571e0a2
Anton <nachalnikkochengui@gmail.com>
2023-03-11 16:37:29 +0800

    P

---
+++ poryadoke/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.WriteLine($"Введите размер массива m , n :");
+int m = Numbers("Введите m: ");
+int n = Numbers("Введите n: ");
+int range = Numbers("Введите числа: от 1 до ");
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine($"Отсортированный массив: ");
+OnemoreArray(array);
+PrintArray(array);
+
+int Numbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+void OnemoreArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
